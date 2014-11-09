965ea3bc7569bd8f35abd2a6ccf49cbbef2b52d2
issam.zeibak <issam.zeibak@gengo.com>
2014-11-09 21:40:00 +0900

    added open id stuffs

--- app/views.py
+++ app/views.py
@@ -1,19 +1,33 @@
-from flask import render_template
+from flask import render_template, flash, redirect
+from forms import LoginForm
 from app import app
 
 @app.route('/')
-@app.route('/index')
 def index():
     user = {'nickname': 'Miguel'}  # fake user
-    title = 'Home'
-    posts = [
-        {
-            'author': {'nickname': 'john'},
-            'body':'Stuff going on'
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
         },
-        {
-            'author': {'nickname': 'dude'},
-            'body':'Stuff going on'
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
         }
     ]
-    return render_template('index.html', title=title, user=user, posts=posts)
+
+    form = LoginForm()
+    if form.validate_on_submit():
+         flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+         return redirect('/index')
+    return render_template('login.html',
+                           title='Sign in',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
